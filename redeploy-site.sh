d044a6d16bef4a17ebadc7336cce3a3ef4d29cae
Joshua Ji <joshua.ji@jiwhiz.com>
2022-06-24 14:51:23 -0600

    Use `.venv` instead of `python3-virtualenv`

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,7 +3,7 @@ tmux kill-ses -t flask-portfolio
 echo "Fetching newest changes..."
 git fetch && git reset origin/main --hard
 echo "Python venv stuff"
-source python3-virtualenv/bin/activate
+source .venv/bin/activate
 pip install -r requirements.txt
 echo "tmux..."
 tmux new-session -d -s flask-portfolio 'yarn dev-server'
