16ba06defba77ac6a7e85279823fd70552a8a23a
Joshua Ji <joshua.ji@jiwhiz.com>
2022-06-26 14:05:59 -0600

    Creating service - part 2

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,6 @@
-echo "Killing all tmux sessions..."
-tmux kill-ses -t flask-portfolio
-echo "Fetching newest changes..."
-git fetch && git reset origin/main --hard
-echo "Python venv stuff"
-source .venv/bin/activate
-pip install -r requirements.txt
-echo "tmux..."
-tmux new-session -d -s flask-portfolio 'yarn dev-server'
-echo "deactivating python shell"
-deactivate
-echo "Finished!" 
+echo "Reloading daemon..."
+systemctl daemon-reload
+echo "Restarting myportfolio"
+systemctl restart myportfolio
+echo "Status"
+systemctl status myportfolio
