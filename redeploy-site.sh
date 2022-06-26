5103d6c3dae3c7f3f9ef0609d7a357495b9ff241
Joshua Ji <joshua.ji@jiwhiz.com>
2022-06-26 14:06:06 -0600

    Creating service - part 2

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,3 +1,9 @@
+cd /root/MLH-portfolio-site
+git fetch && git reset origin/main --hard
+
+source .venv/bin/activate
+pip install -r requirements.txt 
+
 echo "Reloading daemon..."
 systemctl daemon-reload
 echo "Restarting myportfolio"
