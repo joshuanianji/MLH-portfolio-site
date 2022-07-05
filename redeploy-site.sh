06f5e7ed739ab715ca5197c5d1860e4911bb82d3
Joshua Ji <joshua.ji@jiwhiz.com>
2022-07-04 23:28:15 -0700

    Update redeploy site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,5 @@
 cd /root/MLH-portfolio-site
 git fetch && git reset origin/main --hard
 
-source .venv/bin/activate
-pip install -r requirements.txt 
-
-echo "Reloading daemon..."
-systemctl daemon-reload
-echo "Restarting myportfolio"
-systemctl restart myportfolio
-echo "Status"
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
