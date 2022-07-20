c3c5acf4087ed8f86bd7418d9d2a29fa4c821291
Joshua Ji - Oracle Ubuntu <joshua.ji@jiwhiz.com>
2022-07-20 21:33:59 +0000

    Init t3-rewrite

--- redeploy-site.sh
+++
@@ -1,5 +0,0 @@
-cd /root/MLH-portfolio-site
-git fetch && git reset origin/main --hard
-
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
--- run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker compose -f docker-compose.yml run myportfolio python -m unittest -v tests.test_db
-docker compose -f docker-compose.yml run myportfolio python -m unittest -v tests.test_app
