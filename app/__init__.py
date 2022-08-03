91905de8d8faf981bd562ac922bf444954381576
Joshua Ji <joshuanianji@users.noreply.github.com>
2022-08-03 15:49:40 -0600

    Cache pip (#11)
    * Cache pip
    
    * Update something random

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ if os.getenv("TESTING") == "true":
     print('Running in test mode...')
     mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
 else:
-    print('Not running in test mode')
+    print('Not running in test mode...')
     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                          user=os.getenv("MYSQL_USER"),
                          password=os.getenv("MYSQL_PASSWORD"),
